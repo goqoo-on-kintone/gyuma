02f938c0fd3deb0d63d974f8e29eacc16b79c082
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2019-01-21 01:14:20 +0900

    feat: バージョンとヘルプ出力機能

--- bin/gyuma.js
+++ bin/gyuma.js
@@ -5,6 +5,29 @@
 const minimist = require('minimist')
 const server = require('../lib/server')
 
+const trim = str => str.replace(/^\n|\n$/g, '')
+
+const showVersion = () => {
+  const { version } = require('../package.json')
+  console.error(`Gyuma OAuth ${version}`)
+  process.exit(0)
+}
+
+const usageExit = (returnCode = 0) => {
+  const message = trim(`
+usage: gyuma [<options>]
+
+  -h, --help                          Output usage information
+  -v, --version                       Output version information
+  -i, --client_id=<CLIENT_ID>         kintone OAuth2 Client ID
+  -s, --client_secret=<CLIENT_SECRET> kintone OAuth2 Client Secret
+  -S, --scope=<SCOPE>                 kintone OAuth2 Scope
+  -d, --domain=<DOMAIN>               kintone domain name
+  -p, --port=<PORT>                   Web Server port number - defaults to 3000
+`)
+  console.error(message)
+  process.exit(returnCode)
+}
 const parseArgumentOptions = () => {
   const argv = minimist(process.argv.slice(2), {
     boolean: ['version', 'help', 'onetime'],
@@ -25,6 +48,13 @@ const parseArgumentOptions = () => {
 
 const main = async () => {
   const argv = parseArgumentOptions()
+
+  if (argv.version) {
+    showVersion()
+  } else if (argv.help) {
+    usageExit(0)
+  }
+
   try {
     await server(argv)
   } catch (err) {
