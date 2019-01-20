d1c1aa4f2c01637001eefa78eb4cd411b9170a65
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2019-01-20 13:30:18 +0900

    refactor: promisify()をやめてnew Promise()使用

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fetch = require('node-fetch')
 const qs = require('querystring')
 const fs = require('fs')
 const https = require('https')
-const { promisify } = require('util')
 const opener = require('opener')
 require('dotenv').config()
 
@@ -20,63 +19,64 @@ const options = {
 const httpsServer = https.createServer(options, app)
 const port = process.env.HTTPS_PORT || 3000
 
-module.exports = promisify((params, callback) => {
-  const { clientId: client_id, clientSecret: client_secret, scope, domain } = params
-  const localhost = `https://localhost:${port}`
-  /* eslint-disable camelcase */
-  const redirect_uri = `${localhost}/oauth2callback`
-  const response_type = 'code'
-  /* eslint-enable camelcase */
-  const state = Math.random()
-    .toString(36)
-    .slice(-8)
+module.exports = params =>
+  new Promise((resolve, reject) => {
+    const { clientId: client_id, clientSecret: client_secret, scope, domain } = params
+    const localhost = `https://localhost:${port}`
+    /* eslint-disable camelcase */
+    const redirect_uri = `${localhost}/oauth2callback`
+    const response_type = 'code'
+    /* eslint-enable camelcase */
+    const state = Math.random()
+      .toString(36)
+      .slice(-8)
 
-  const authUri = `https://${domain}/oauth2/authorization`
-  const tokenUri = `https://${domain}/oauth2/token`
+    const authUri = `https://${domain}/oauth2/authorization`
+    const tokenUri = `https://${domain}/oauth2/token`
 
-  // 認可要求
-  app.get('/', (req, res) => {
-    const params = qs.stringify({
-      client_id,
-      redirect_uri,
-      state,
-      response_type,
-      scope,
+    // 認可要求
+    app.get('/', (req, res) => {
+      const params = qs.stringify({
+        client_id,
+        redirect_uri,
+        state,
+        response_type,
+        scope,
+      })
+      res.redirect(302, `${authUri}?${params}`)
     })
-    res.redirect(302, `${authUri}?${params}`)
+
+    // アクセストークンの要求・取得
+    app.get('/oauth2callback', async (req, res) => {
+      try {
+        res.header('Content-Type', 'application/json; charset=utf-8')
+        const resp = await fetch(tokenUri, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: qs.stringify({
+            client_id,
+            client_secret,
+            code: req.query.code,
+            grant_type: 'authorization_code',
+            redirect_uri,
+          }),
+        }).then(res => res.json())
+
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+        res.write('Authentication succeeded 🎉' + '</br>')
+        res.end('Close the browser and return to the Terminal.')
+        server.close()
+
+        await resolve(resp)
+      } catch (err) {
+        await reject(err)
+      }
+    })
+
+    const server = httpsServer.listen(port, () => {
+      console.log(`listening on ${localhost}`)
+      opener(localhost)
+    })
   })
-
-  // アクセストークンの要求・取得
-  app.get('/oauth2callback', async (req, res) => {
-    try {
-      res.header('Content-Type', 'application/json; charset=utf-8')
-      const resp = await fetch(tokenUri, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: qs.stringify({
-          client_id,
-          client_secret,
-          code: req.query.code,
-          grant_type: 'authorization_code',
-          redirect_uri,
-        }),
-      }).then(res => res.json())
-
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-      res.write('Authentication succeeded 🎉' + '</br>')
-      res.end('Close the browser and return to the Terminal.')
-      server.close()
-
-      await callback(null, resp)
-    } catch (err) {
-      await callback(err)
-    }
-  })
-
-  const server = httpsServer.listen(port, () => {
-    console.log(`listening on ${localhost}`)
-    opener(localhost)
-  })
-})
