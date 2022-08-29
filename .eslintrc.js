2a67c91d899f50f92cae3a42bc24626772f8c720
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-29 14:25:21 +0900

    chore: Node.jsバージョンアップ、ESLint設定調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['standard', 'plugin:n/recommended', 'prettier'],
+  parserOptions: { ecmaVersion: 2022 },
   rules: {
     'no-var': 'error',
     'prefer-const': 'error',
