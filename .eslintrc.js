4d5e20869434bd071ce8e83a7f447a65ebbd14fb
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-30 12:13:04 +0900

    chore: Install jest, typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
-  extends: ['standard', 'plugin:n/recommended', 'prettier'],
+  env: {
+    'node': true,
+    'jest/globals': true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'prettier'],
   parserOptions: { ecmaVersion: 2022 },
   rules: {
     'no-var': 'error',
     'prefer-const': 'error',
     'n/no-process-exit': 'off',
     'camelcase': 'off',
+
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'no-irregular-whitespace': ['error', { skipTemplates: true }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
   },
 }
